7b1698959516f81cfe916291bf6a5784a8f0665e
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2022-01-15 16:22:51 -0300

    Create a simple desktop app in order to test the portable db

--- src/facturas/app/Main.java
+++ src/facturas/app/Main.java
@@ -5,40 +5,18 @@
  */
 package facturas.app;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 /**
  *
  * @author Agustin
  */
 public class Main {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("facturas-appPU");
-        FacturaJpaController jpa = new FacturaJpaController(emf);
-        Factura factura = new Factura();
-        try {
-            jpa.create(factura);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        
-        for(Factura f : jpa.findFacturaEntities()) {
-            System.out.println(f);
-            /*try {
-                jpa.destroy(f.getId());
-            } catch (Exception e) {
-                System.out.println(e);
-            }*/
-        }
-        
         MainWindow mw = new MainWindow();
         mw.setVisible(true);
-        
     }
     
 }
--- src/facturas/app/MainWindow.java
+++ src/facturas/app/MainWindow.java
@@ -5,11 +5,20 @@
  */
 package facturas.app;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Agustin
  */
 public class MainWindow extends javax.swing.JFrame {
+    
+    public static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+    public static final String JDBC_URL = "jdbc:derby:db;create=true";
 
     /**
      * Creates new form MainWindow
@@ -29,10 +38,12 @@ public class MainWindow extends javax.swing.JFrame {
 
         jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("Boton");
+        jButton1.setText("Crear Tabla");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -41,25 +52,48 @@ public class MainWindow extends javax.swing.JFrame {
 
         jLabel1.setText("Hola");
 
+        jButton2.setText("Agregar Comprobante");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("Borrar Borrar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(187, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
-                    .addComponent(jButton1))
-                .addGap(152, 152, 152))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(36, 36, 36)
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 95, Short.MAX_VALUE)
+                .addComponent(jButton2)
+                .addGap(106, 106, 106)
+                .addComponent(jButton3)
+                .addGap(46, 46, 46))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(210, 210, 210)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(81, 81, 81)
+                .addGap(146, 146, 146)
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 113, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(69, 69, 69))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 179, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jButton2)
+                    .addComponent(jButton3))
+                .addGap(30, 30, 30))
         );
 
         pack();
@@ -67,7 +101,66 @@ public class MainWindow extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        //stm.executeUpdate("DROP TABLE Factura");
+        try {    
+            Class.forName(DRIVER);
+            Connection connection = DriverManager.getConnection(JDBC_URL);
+            
+            Statement stm = connection.createStatement();
+            //stm.executeUpdate("DROP TABLE Factura");
+            
+            String table = "CREATE TABLE Factura ("
+                        + "ID INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,"
+                        + "PROVEEDOR VARCHAR(45) NOT NULL)";
+            stm.executeUpdate(table);
+            
+            jLabel1.setText("Tabla creada");
+        } catch (Exception e) {
+            jLabel1.setText(e.toString());
+        }
+        
+
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        try {
+            Class.forName(DRIVER);
+            Connection connection = DriverManager.getConnection(JDBC_URL);
+            Statement stm = connection.createStatement();
+            
+            String query = "INSERT INTO Factura(PROVEEDOR) VALUES ('Agus')";
+            stm.executeUpdate(query);
+
+            ResultSet result = stm.executeQuery("SELECT * FROM Factura");            
+
+            String str = "";
+            while(result.next()) {
+                for(int i = 1; i <= result.getMetaData().getColumnCount(); i++) {
+                    str += result.getString(i) + " | ";
+                }
+                str += "\n";
+            }
+            jLabel1.setText(str);
+            
+        } catch (Exception e) {
+            jLabel1.setText(e.toString());
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        try {    
+            Class.forName(DRIVER);
+            Connection connection = DriverManager.getConnection(JDBC_URL);
+            
+            Statement stm = connection.createStatement();
+            stm.executeUpdate("DROP TABLE Factura");
+            jLabel1.setText("Tabla borrada");
+        } catch (Exception e) {
+            jLabel1.setText(e.toString());
+        }
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -106,6 +199,8 @@ public class MainWindow extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
