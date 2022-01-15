e27456ff1cd47f1a21e13ffc4df75d44183ed31d
augustofungo@gmail.com <>
2022-01-15 19:14:44 -0300

    Buil elemental metods of class Provider.java. Ref #3

---
+++ src/facturas/app/Provider.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package facturas.app;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Provider {
+    private int cuit;
+    private String name;
+
+    public Provider(int cuit, String name){
+        this.cuit = cuit;
+        this.name = name;
+    }
+    public int getCuit(){
+        return cuit;
+    }
+    public String getName(){
+        return name;
+    }
+    
+    public String toString(){
+        return "Nombre" + name + "    " + "Cuit" + cuit; 
+    }
+}
