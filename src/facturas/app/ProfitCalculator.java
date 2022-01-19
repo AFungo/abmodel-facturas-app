848e42c514171d1f8da7026e734087282efe9cf7
yeografik <nachogonz2702@gmail.com>
2022-01-19 12:22:18 -0300

    New class ProfitCalculator #11

---
+++ src/facturas/app/ProfitCalculator.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facturas.app;
+
+/**
+ *
+ * @author nacho
+ */
+public class ProfitCalculator {
+    
+    private Purchase purchases;
+    private Sale sales;
+    
+    public ProfitCalculator() {
+        purchases = new Purchase(0, 0);
+        sales = new Sale(0, 0, 0, 0);
+    }
+    
+    public void addTicket(Ticket t) {
+        throw new UnsupportedOperationException("to do");
+    }
+    
+}
