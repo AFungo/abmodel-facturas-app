af4ce1d2ce7af1617f84fcd8e7945849d40ff2bf
yeografik <nachogonz2702@gmail.com>
2022-01-19 12:21:34 -0300

    New class Purchase #4

---
+++ src/facturas/app/Purchase.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facturas.app;
+
+/**
+ *
+ * @author nacho
+ */
+public class Purchase {
+    
+    private float totalAmount;
+    private float iva;
+    
+    public Purchase(float totalAmount, float iva) {
+        this.totalAmount = totalAmount;
+        this.iva = iva;
+    }
+    
+    public void addPurchase(float amount, float iva) {
+        this.totalAmount += amount;
+        this.iva += iva;
+    }
+    
+    public float [ ] getPurchases() {
+        float [ ] purchases = {totalAmount, iva};
+        return purchases;
+    }
+}
