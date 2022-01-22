9151b1edbf6d39431f902b73b15bceda529e4840
yeografik <nachogonz2702@gmail.com>
2022-01-21 21:33:12 -0300

    Added file validation check to controller #16

--- src/facturas/app/Controller.java
+++ src/facturas/app/Controller.java
@@ -13,7 +13,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -33,7 +32,12 @@ public class Controller {
             Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        stringTickets.remove(0); //Remove the first row of the file
+        String initialLine = stringTickets.remove(0); //Remove the first row of the file for checking
+        if (!validFormat(initialLine)) {
+            System.out.println("File does not have a valid format to be loaded\nFile: " + fileName);
+            return;
+        }
+            
         List<Ticket> tickets = new ArrayList<>();
         for (String strTicket : stringTickets) {
             String[ ] ticketAttributes = strTicket.replace("\"", "").split(",");
@@ -59,4 +63,12 @@ public class Controller {
         return TicketDAO.getTickets(filters);
     }
     
+    private boolean validFormat(String initialLine) {
+        String expectedLine = "\"Fecha\",\"Tipo\",\"Punto de Venta\",\"Número Desde\",\"Número Hasta\",\"Cód. Autorización\",\"Tipo Doc. Emisor\",\"Nro. Doc. Emisor\",\"Denominación Emisor\",\"Tipo Cambio\",\"Moneda\",\"Imp. Neto Gravado\",\"Imp. Neto No Gravado\",\"Imp. Op. Exentas\",\"IVA\",\"Imp. Total\"";
+        char initialChar = initialLine.charAt(0);
+        if ((int)initialChar == 65279)  //special char that may come with utf-8 files
+            initialLine = initialLine.substring(1); //remove special char
+        
+        return initialLine.contentEquals(expectedLine);
+    }
 }
