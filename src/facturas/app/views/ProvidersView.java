14f3bfcb9223b401a6013a19dcd97aa68b770e23
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2022-01-26 10:04:53 -0300

    Create new class AutoSuggestor for extract the behaviour from the view. Ref #22

--- src/facturas/app/views/ProvidersView.java
+++ src/facturas/app/views/ProvidersView.java
@@ -8,13 +8,9 @@ package facturas.app.views;
 import facturas.app.Controller;
 import facturas.app.database.ProviderDAO;
 import facturas.app.models.Provider;
-import java.awt.EventQueue;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.util.Collections;
+import facturas.app.utils.AutoSuggestor;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Vector;
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
@@ -24,17 +20,22 @@ import javax.swing.table.DefaultTableModel;
  */
 public class ProvidersView extends javax.swing.JFrame {
 
-    private Controller controller;
-    private JTextField nameTextField;
-    
     /**
      * Creates new form ProvidersView
      */
     public ProvidersView(Controller controller) {
         this.controller = controller;
         initComponents();
-        loadProviders();
-        autoSuggest();
+        autoSuggestor = new AutoSuggestor(comboBox, nameTextField, getProvidersName());
+        autoSuggestor.autoSuggest();
+    }
+    
+    private List<String> getProvidersName() {
+        List<String> names = new LinkedList<>();
+        for (Provider p : controller.getProviders()) {
+            names.add(p.getName());
+        }
+        return names;
     }
 
     /**
@@ -48,8 +49,9 @@ public class ProvidersView extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         providersTable = new javax.swing.JTable();
-        showProviders = new javax.swing.JButton();
+        searchProvider = new javax.swing.JButton();
         comboBox = new javax.swing.JComboBox<>();
+        showAllProviders = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -92,11 +94,18 @@ public class ProvidersView extends javax.swing.JFrame {
         providersTable.setCellSelectionEnabled(true);
         providersTable.setVisible(true);
 
-        showProviders.setText("Mostrar proveedores");
-        showProviders.setPreferredSize(new java.awt.Dimension(140, 23));
-        showProviders.addActionListener(new java.awt.event.ActionListener() {
+        searchProvider.setText("Buscar proveedor");
+        searchProvider.setPreferredSize(new java.awt.Dimension(140, 23));
+        searchProvider.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                showProvidersActionPerformed(evt);
+                searchProviderActionPerformed(evt);
+            }
+        });
+
+        showAllProviders.setText("Mostrar proveedores");
+        showAllProviders.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                showAllProvidersActionPerformed(evt);
             }
         });
 
@@ -108,11 +117,13 @@ public class ProvidersView extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jScrollPane1)
                 .addContainerGap())
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(85, Short.MAX_VALUE)
                 .addComponent(comboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(63, 63, 63)
-                .addComponent(showProviders, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(showAllProviders, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(searchProvider, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(112, 112, 112))
         );
         layout.setVerticalGroup(
@@ -122,122 +133,49 @@ public class ProvidersView extends javax.swing.JFrame {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 226, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(showProviders, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(searchProvider, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(comboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(50, 50, 50))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(showAllProviders)
+                .addGap(21, 21, 21))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void showProvidersActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_showProvidersActionPerformed
+    private void searchProviderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchProviderActionPerformed
         // TODO add your handling code here:
         DefaultTableModel model = (DefaultTableModel)providersTable.getModel();
         cleanTable(model);
-        List<Provider> providers = ProviderDAO.getProviders(nameTextField.getText());
+        List<Provider> providers = ProviderDAO.getProviders(autoSuggestor.getText());
         for (Provider p : providers) {
             model.addRow(new Object[] {p.getCuit(), p.getName(), p.getDocType()});
         }
-    }//GEN-LAST:event_showProvidersActionPerformed
+    }//GEN-LAST:event_searchProviderActionPerformed
+
+    private void showAllProvidersActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_showAllProvidersActionPerformed
+        // TODO add your handling code here:
+        DefaultTableModel model = (DefaultTableModel)providersTable.getModel();
+        cleanTable(model);
+        for (Provider p : controller.getProviders()) {
+            model.addRow(new Object[] {p.getCuit(), p.getName(), p.getDocType()});
+        }
+    }//GEN-LAST:event_showAllProvidersActionPerformed
 
     private void cleanTable(DefaultTableModel model) {
         for (int i = model.getRowCount() - 1; 0 <= i; i--)
             model.removeRow(i);
     }
-
-    private final Vector<String> v = new Vector<>();
-    private boolean hide_Flag = false;
-    
-    private void loadProviders() {
-        List<Provider> providers = controller.getProviders();
-        for (Provider p : providers) {
-            v.add(p.getName());
-        }
-    }
-    
-    private void setModel(DefaultComboBoxModel model, String str) {
-        comboBox.setModel(model);
-        comboBox.setSelectedIndex(-1);
-        nameTextField.setText(str);
-    }
-    
-    private static DefaultComboBoxModel getSuggestedModel(List<String> list, String text) {
-        DefaultComboBoxModel m = new DefaultComboBoxModel();
-        for (String s : list) {
-            if (s.startsWith(text)) {
-                m.addElement(s);
-            }
-        }
-        
-        return m;
-    }
-    
-    private void autoSuggest() {
-        nameTextField = new JTextField();
-        nameTextField.setText("");
-        comboBox.setEditable(true);
-        nameTextField = (JTextField)comboBox.getEditor().getEditorComponent();
-        nameTextField.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {
-                EventQueue.invokeLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        String text = nameTextField.getText();
-                        if (text.length() == 0) {
-                            comboBox.hidePopup();
-                            setModel(new DefaultComboBoxModel(v), text);
-                        } else {
-                            DefaultComboBoxModel m = getSuggestedModel(v, text);
-                            if (m.getSize() == 0 || hide_Flag) {
-                                comboBox.hidePopup();
-                                hide_Flag = false;
-                            } else {
-                                setModel(m, text);
-                                comboBox.showPopup();
-                            }
-                        }
-                    }
-                
-                });
-            }
-            
-            @Override
-            public void keyPressed(KeyEvent e) {
-                String text = nameTextField.getText();
-                int code = e.getKeyCode();
-                if (code == KeyEvent.VK_ENTER) {
-                    if (v.contains(text)) {
-                        v.addElement(text);
-                        Collections.sort(v);
-                        setModel(getSuggestedModel(v, text), text);
-                    }
-                    hide_Flag = true;
-                } else if (code == KeyEvent.VK_ESCAPE) {
-                    hide_Flag = true;
-                } else if (code == KeyEvent.VK_RIGHT) {
-                    for (int i = 0; i < v.size(); i++) {
-                        String str = v.elementAt(i);
-                        if (str.startsWith(text)) {
-                            comboBox.setSelectedIndex(-1);
-                            nameTextField.setText(str);
-                            return;
-                        }
-                    }
-                }
-            }
-            
-            @Override
-            public void keyReleased(KeyEvent e) {
-            }
-        });
-    }
     
     
+    private Controller controller;
+    private AutoSuggestor autoSuggestor;
+    private JTextField nameTextField;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox<String> comboBox;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable providersTable;
-    private javax.swing.JButton showProviders;
+    private javax.swing.JButton searchProvider;
+    private javax.swing.JButton showAllProviders;
     // End of variables declaration//GEN-END:variables
 }
