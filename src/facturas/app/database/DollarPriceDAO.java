83065c76b80a510623602f0e6418212cbc29a61c
yeografik <nachogonz2702@gmail.com>
2022-01-27 19:23:46 -0300

    New method for creating the DollarPrice and for getting a relative price if the requested date doesn't have one #26

--- src/facturas/app/database/DollarPriceDAO.java
+++ src/facturas/app/database/DollarPriceDAO.java
@@ -32,17 +32,50 @@ public class DollarPriceDAO extends DAO {
     
     public static DollarPrice getPrice(Date date) {
         ResultSet result = executeQuery("SELECT * FROM DollarPrice WHERE date = '" + date.toString() + "'", false);
+        DollarPrice price = buildDollarPrice(result);
+        return price;
+    }
+    
+    public static DollarPrice getAproximatePrice(Date date) {
+        ResultSet before = executeQuery("SELECT * FROM DollarPrice WHERE date = ("
+                + "SELECT MAX(date) FROM DollarPrice WHERE date < '" + date.toString() + "')", false);
+        ResultSet after = executeQuery("SELECT * FROM DollarPrice WHERE date > '" + date.toString() + "' FETCH FIRST 1 ROWS ONLY", false);
+        
+        DollarPrice priceBefore = buildDollarPrice(before);
+        DollarPrice priceAfter = buildDollarPrice(after);
+        
+        //we assume that the user will load more than 1 sigle price, so this will always find another date
+        if (priceBefore == null)
+            return priceAfter;
+        else if (priceAfter == null)
+            return priceBefore;
+        else {
+            Date dateAfter = priceAfter.getDate();
+            Date dateBefore = priceBefore.getDate();
+            //getting time of each date
+            long currentTime = date.getTime();
+            long afterTime = dateAfter.getTime();
+            long beforeTime = dateBefore.getTime();
+            //getting difference between both dates
+            long afterAbs = Math.abs(afterTime - currentTime);
+            long beforeAbs = Math.abs(currentTime - beforeTime);
+            //return the DollarPrice of the nearest date
+            DollarPrice finalPrice = afterAbs <= beforeAbs ? priceAfter : priceBefore;
+            return finalPrice;
+        }
+    }
+    
+    private static DollarPrice buildDollarPrice(ResultSet result) {
         DollarPrice price = null;
         try {
             if (!result.next()) {
                 return null;
-            } else {
-                Map<String, String> priceAttributes = new HashMap<>();
-                priceAttributes.put("date", result.getString(1));
-                priceAttributes.put("buy", result.getString(2));
-                priceAttributes.put("sell", result.getString(3));
-                price = new DollarPrice(priceAttributes);
             }
+            Map<String, String> priceAttributes = new HashMap<>();
+            priceAttributes.put("date", result.getString(1));
+            priceAttributes.put("buy", result.getString(2));
+            priceAttributes.put("sell", result.getString(3));
+            price = new DollarPrice(priceAttributes);
         } catch (SQLException ex) {
             Logger.getLogger(DollarPriceDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
