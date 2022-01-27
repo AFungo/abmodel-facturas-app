28f5063f6e6a08fd3904c343fecae9932e2e96b1
yeografik <nachogonz2702@gmail.com>
2022-01-26 21:38:03 -0300

    new class DollarPriceDAO #26

---
+++ src/facturas/app/database/DollarPriceDAO.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facturas.app.database;
+
+import static facturas.app.database.DAO.executeQuery;
+import facturas.app.models.DollarPrice;
+import facturas.app.utils.Formater;
+import facturas.app.utils.Pair;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author nacho
+ */
+public class DollarPriceDAO extends DAO {
+    
+    public static void addDollarPrice(DollarPrice price) {
+        Pair<String, String> sqlValues = Formater.dollarPriceToSQL(price);
+        String query = "INSERT INTO DollarPrice (" + sqlValues.getFst() + ") "
+            + "VALUES (" + sqlValues.getSnd() + ")";
+        executeQuery(query, true);
+    }
+    
+    public static DollarPrice getPrice(Date date) {
+        ResultSet result = executeQuery("SELECT * FROM Ticket WHERE date = " + date, false);
+        DollarPrice price = null;
+        try {
+            if (!result.next())
+                return null;
+            else {
+                Map<String, String> priceAttributes = new HashMap<>();
+                priceAttributes.put("date", result.getString(2));
+                priceAttributes.put("buy", result.getString(3));
+                priceAttributes.put("sell", result.getString(4));
+                price = new DollarPrice(priceAttributes);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DollarPriceDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return price;
+    }
+}
