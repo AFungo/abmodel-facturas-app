da94901aab869d50027090d2b3ce5e7fffa41254
yeografik <nachogonz2702@gmail.com>
2022-01-31 00:27:58 -0300

    New class ConfigManager to read and save settings #39

---
+++ src/facturas/app/utils/ConfigManager.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facturas.app.utils;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author nacho
+ */
+public class ConfigManager {
+ 
+    final static String configFolderPath = System.getenv("APPDATA") + "\\Facturas App\\";
+    final static String configPath = System.getenv("APPDATA") + "\\Facturas App\\" + "config.txt";
+    
+    public static Map<String, Boolean> readConfig() {
+        File configsFile = new File(configPath);
+        if ((!configsFile.exists()) || configsFile.length() == 0) {
+            initializeConfig();
+            saveConfig(initialConfig());
+        }
+        
+        List<String> stringItems = new LinkedList<>();
+        try {
+            stringItems = Files.readAllLines(configsFile.toPath(), Charset.defaultCharset());
+            stringItems.remove(0);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        Map<String, Boolean> configs = new HashMap<String, Boolean>();
+        for (String line : stringItems) {
+            if (line.equals("[selected columns]")) continue;
+            
+            String[] values = line.split("=");
+            configs.put(values[0], new Boolean(values[1]));
+        }
+        return configs;
+    }
+
+    public static void saveConfig(Map<String, Boolean> configs) {
+        String fileContent = "\n[selected columns]";
+        for (Map.Entry<String, Boolean> line : configs.entrySet()) {
+            fileContent += "\n" + line.getKey() + "=" + line.getValue();
+        }
+        
+        try {
+            FileWriter writer = new FileWriter(configPath);
+            writer.write(fileContent);
+            writer.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    private static void initializeConfig() {
+        File configsFolder = new File(configFolderPath);
+        if (!configsFolder.exists()) {    //creating directory if not exists
+            configsFolder.mkdir();
+        }
+        
+        File configsFile = new File(configPath);
+        try {
+            configsFile.createNewFile();    //creating file
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    private static Map<String, Boolean> initialConfig() {
+        Map<String, Boolean> configs = new HashMap<String, Boolean>();
+        configs.put("date", Boolean.TRUE);
+        configs.put("type", Boolean.TRUE);
+        configs.put("noTicket", Boolean.TRUE);
+        configs.put("numberTo", Boolean.TRUE);
+        configs.put("authCode", Boolean.TRUE);
+        configs.put("noCuit", Boolean.TRUE);
+        configs.put("providerName", Boolean.TRUE);
+        configs.put("changeType", Boolean.TRUE);
+        configs.put("netAmountWI", Boolean.TRUE);
+        configs.put("netAmountWOI", Boolean.TRUE);
+        configs.put("amountImpEx", Boolean.TRUE);
+        configs.put("iva", Boolean.TRUE);
+        configs.put("ticketSector", Boolean.TRUE);
+        configs.put("totalAmount", Boolean.TRUE);
+        configs.put("cuit", Boolean.TRUE);
+        configs.put("name", Boolean.TRUE);
+        configs.put("docType", Boolean.TRUE);
+        configs.put("direction", Boolean.TRUE);
+        configs.put("providerSector", Boolean.TRUE);
+        return configs;
+    }
+}
