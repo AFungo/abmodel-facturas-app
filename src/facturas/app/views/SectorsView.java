5909fcc6b7e3a85d3cf590b55d818c9bf26b5e76
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2022-02-04 21:35:11 -0300

    Add a update button for change the name of a sector. Ref #44

--- src/facturas/app/views/SectorsView.java
+++ src/facturas/app/views/SectorsView.java
@@ -1,12 +1,17 @@
-/*
+/*S
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package facturas.app.views;
 
+import facturas.app.database.ProviderDAO;
+import facturas.app.database.SQLFilter;
 import facturas.app.database.SectorDAO;
+import facturas.app.database.TicketDAO;
+import facturas.app.models.Ticket;
 import facturas.app.utils.AutoSuggestor;
 import java.util.List;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 /**
@@ -42,9 +47,11 @@ public class SectorsView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        updateSectorOptionPane = new javax.swing.JOptionPane();
         addSector = new javax.swing.JButton();
         deleteSector = new javax.swing.JButton();
         sectorsComboBox = new javax.swing.JComboBox<>();
+        updateSector = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -62,33 +69,42 @@ public class SectorsView extends javax.swing.JFrame {
             }
         });
 
-        sectorsComboBox.setSelectedIndex(-1);
+        updateSector.setText("Actualizar");
+        updateSector.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                updateSectorActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(33, Short.MAX_VALUE)
                 .addComponent(addSector, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                .addComponent(updateSector, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
                 .addComponent(deleteSector, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(33, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(sectorsComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(sectorsComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
+                .addContainerGap(19, Short.MAX_VALUE)
                 .addComponent(sectorsComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGap(18, 31, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(addSector)
-                    .addComponent(deleteSector))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(deleteSector)
+                        .addComponent(updateSector)))
+                .addContainerGap(20, Short.MAX_VALUE))
         );
 
         pack();
@@ -112,11 +128,26 @@ public class SectorsView extends javax.swing.JFrame {
         // be removed
     }//GEN-LAST:event_deleteSectorActionPerformed
 
+    private void updateSectorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_updateSectorActionPerformed
+        String userInput = updateSectorOptionPane.showInputDialog(null, "Nuevo nombre: ", "");
+        if (userInput != null && sectorsAutoSuggestor.getText() != null) {
+            SQLFilter filter = new SQLFilter();
+            filter.add("sector", "=", sectorsAutoSuggestor.getText(), String.class);
+            SectorDAO.addSector(userInput);
+            TicketDAO.changeSector(filter, userInput);
+            SectorDAO.deleteSector(sectorsAutoSuggestor.getText());
+            sectorsAutoSuggestor.setText("");
+            updateSuggestions();
+        }
+    }//GEN-LAST:event_updateSectorActionPerformed
+
     JTextField sectorsTextField;
     AutoSuggestor sectorsAutoSuggestor;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton addSector;
     private javax.swing.JButton deleteSector;
     private javax.swing.JComboBox<String> sectorsComboBox;
+    private javax.swing.JButton updateSector;
+    private javax.swing.JOptionPane updateSectorOptionPane;
     // End of variables declaration//GEN-END:variables
 }
