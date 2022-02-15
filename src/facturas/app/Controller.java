5b20c977750f99b87824fb560e1491a4bca5c247
yeografik <nachogonz2702@gmail.com>
2022-02-15 15:47:24 -0300

    Added a new method to create a table with a single row to show #77

--- src/facturas/app/Controller.java
+++ src/facturas/app/Controller.java
@@ -278,6 +278,15 @@ public class Controller {
         return table;
     }
     
+    public JTable createTableToDelete(Object[] rowToDelete) {
+        Object[][] rows = {rowToDelete};
+        Object[] cols = {"id", "Fecha", "Tipo", "Nro factura", "Denominación Emisor", "Imp. Total"};
+        JTable table = new JTable(rows, cols);
+        table.setDefaultEditor(Object.class, null);
+        table.setCellSelectionEnabled(true);
+        return table;
+    }
+    
     public void cleanTextField(JTextField[] textField){
             for(JTextField t : textField) t.setText("");
     }
