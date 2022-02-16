e037c4d8e02aa981e6c65c6f76cfed19b77a3d22
yeografik <nachogonz2702@gmail.com>
2022-02-16 18:41:29 -0300

    New class to hold fixed data #70

---
+++ src/facturas/app/utils/FixedData.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facturas.app.utils;
+
+/**
+ *
+ * @author nacho
+ */
+public class FixedData {
+    
+    public static String[] getTicketTypes() {
+        String[] types = {"1 - Facturas A", "2 - Notas de Debito A", "3 - Notas de Credito A", "4 - Recibos A", 
+            "5 - Notas de Venta al Contado A", "6 - Facturas B", "7 - Notas de Debito B", "8 - Notas de Credito B", 
+            "9 - Recibos B", "10 - Notas de Venta al Contado B", "11 - Facturas C", "12 - Notas de Debito C", 
+            "13 - Notas de Credito C", "15 - Recibos C", "16 - Notas de Venta al Contado C", 
+            "17 - Liquidacion de Servicios Publicos Clase A", "18 - Liquidacion de Servicios Publicos Clase B", 
+            "33 - Liquidacion Primaria de Granos", "47 - Nota de Debito Liquidacion Unica Comercial Impositiva Clase C", 
+            "48 - Nota de Credito Liquidacion Unica Comercial Impositiva Clase A", 
+            "49 - Comprobantes de Compra de Bienes no Registrables a Consumidores Finales", 
+            "50 - Recibo Factura a Regimen de Factura de Credito ", "51 - Facturas M", "52 - Notas de Debito M", 
+            "53 - Notas de Credito M", "54 - Recibos M", "55 - Notas de Venta al Contado M", "59 - Liquidaciones M", 
+            "63 - Liquidaciones A", "64 - Liquidaciones B", "68 - Liquidacion C", "70 - Recibos Factura de Credito", 
+            "81 - Tique Factura A", "82 - Tique Factura B", "83 - Tique", "88 - Remito Electronico", "110 - Tique Nota de Credito", 
+            "111 - Tique Factura C", "112 - Tique Nota de Credito A", "113 - Tique Nota de Credito B", "114 - Tique Nota de Credito C", 
+            "115 - Tique Nota de Debito A", "116 - Tique Nota de Debito B", "117 - Tique Nota de Debito C", "118 - Tique Factura M", 
+            "119 - Tique Nota de Credito M", "120 - Tique Nota de Debito M", "331 - Liquidacion Secundaria de Granos", 
+            "332 - Certificacion Electronica (Granos)"};
+        
+        return types;
+    }
+    
+}
