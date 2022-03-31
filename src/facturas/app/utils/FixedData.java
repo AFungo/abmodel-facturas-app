609f828fb97b83274677c55b192e0e76f96a656f
Augusto <abmodel@gmail.com>
2022-03-31 18:14:36 -0300

    I add documentation to fixed data class. Ref #121

--- src/facturas/app/utils/FixedData.java
+++ src/facturas/app/utils/FixedData.java
@@ -8,11 +8,13 @@ package facturas.app.utils;
 import java.util.Arrays;
 
 /**
- *
- * @author nacho
+ * ¸this class give the correct structure of the data
  */
 public class FixedData {
     
+    /*
+        names of all tickets types
+    */
     private static final String[] types = {"1 - Factura A", "2 - Nota de Debito A", "3 - Nota de Credito A", "4 - Recibo A", 
             "5 - Nota de Venta al Contado A", "6 - Factura B", "7 - Nota de Debito B", "8 - Nota de Credito B", 
             "9 - Recibo B", "10 - Nota de Venta al Contado B", "11 - Factura C", "12 - Nota de Debito C", 
@@ -30,42 +32,74 @@ public class FixedData {
             "119 - Tique Nota de Credito M", "120 - Tique Nota de Debito M", "331 - Liquidacion Secundaria de Granos", 
             "332 - Certificacion Electronica (Granos)"};
     
+    /**
+     * @return ticket types 
+     */
     public static String[] getTicketTypes() {
         return types;
     }
 
+    /**
+     * @return document types 
+     */
     public static String[] getDocumentTypes() {
         return new String[] {"CUIT", "CUIL"};
     }
     
+    /**
+     * @return ticket emitter file format 
+     */
     public static String getTicketEmitterFileFormat() {
         return "\"Fecha\",\"Tipo\",\"Punto de Venta\",\"Número Desde\",\"Número Hasta\",\"Cód. Autorización\",\"Tipo Doc. Emisor\",\"Nro. Doc. Emisor\",\"Denominación Emisor\",\"Tipo Cambio\",\"Moneda\",\"Imp. Neto Gravado\",\"Imp. Neto No Gravado\",\"Imp. Op. Exentas\",\"IVA\",\"Imp. Total\"";
     }
     
+    /**
+     * @return ticket receptor file format
+     */ 
     public static String getTicketReceptorFileFormat() {
         return "\"Fecha\",\"Tipo\",\"Punto de Venta\",\"Número Desde\",\"Número Hasta\",\"Cód. Autorización\",\"Tipo Doc. Receptor\",\"Nro. Doc. Receptor\",\"Denominación Receptor\",\"Tipo Cambio\",\"Moneda\",\"Imp. Neto Gravado\",\"Imp. Neto No Gravado\",\"Imp. Op. Exentas\",\"IVA\",\"Imp. Total\"";
     }
     
+    /**
+     * @return dollar price file format
+     */
     public static String getDollarPriceFileFormat() {
         return "Fecha cotizacion;Compra;Venta;";
     }
     
+    /**
+     * @return the format of the ticket 
+     */
     public static String getTicketAppFormat() {
         return "date;number;providerDoc;iva;profits;delivered;sector;type;numberTo;authCode;exchangeType;exchangeMoney;netAmountWI;netAmountWOI;amountImpEx;ivaTax;totalAmount;issuedByMe;";
     }
     
+    /**
+     *  @return the format of the withholdings
+     */
     public static String getWithholdingAppFormat() {
         return "date;number;providerDoc;iva;profits;sector;delivered;";
     }
     
+    /**
+     * @return the format of the provider 
+     */
     public static String getProviderAppFormat() {
         return "docNo;name;direction;sector;alias;documentType;";
     }
     
+    /**
+     * @return the format of the sector
+     */
     public static String getSectorAppFormat() {
         return "name;";
     }
     
+    /**
+     * check if the value of a string is a valid ticket type
+     * @param type the string who want to check if is a valid type
+     * @return a boolean who said is valid or not
+     */
     public static boolean validType(String type) {
         boolean value = Arrays.asList(types).contains(type);
         return value;
