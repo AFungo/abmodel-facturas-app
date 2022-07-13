d0c2b4d4c5165bd188b8895226709835a9edb682
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2022-07-13 09:30:42 -0300

    Add a new map to store the types of the fields, is useful for repOk, and maybe will be useful later. #162

--- src/main/java/facturas/app/models/Provider.java
+++ src/main/java/facturas/app/models/Provider.java
@@ -1,5 +1,6 @@
 package facturas.app.models;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -13,10 +14,27 @@ import java.util.stream.Stream;
 public class Provider {
 
     Map<String, Object> values;
+    Map<String, Class<?>> types;
 
     public Provider(Map<String, Object> values) {
         this.values = new HashMap<>(values);
+        setTypes();
         assert repOk();
+    }
+
+    private void setTypes() {
+        types = new HashMap<String, Class<?>>() {{
+            put("docNo", String.class);
+            put("name", String.class);
+            put("docType", String.class);
+            put("address", String.class);
+            put("provSector", String.class);
+            put("alies", String.class);
+        }};
+    }
+
+    public Map<String, Class<?>> getTypes() {
+        return new HashMap<>(types);
     }
 
     public void setValues(Map<String, Object> values) {
@@ -32,10 +50,18 @@ public class Provider {
 
     private boolean repOk() {
         Set<String> requiredKeys = Stream.of
-                ("docNo", "name", "docType", "direction", "provSector", "alias")
+                ("docNo", "name", "docType", "address", "provSector", "alias")
                 .collect(Collectors.toSet());
         if (!requiredKeys.containsAll(values.keySet())) {
             return false;
+        }
+        for (String key : values.keySet()) {
+            Object value = values.get(key);
+            if (value != null) {
+                if (value.getClass() != types.get(key)) {
+                    return false;
+                }
+            }
         }
         return values.get("docNo") != null && values.get("name") != null && values.get("docType") != null;
     }
