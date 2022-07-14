ddda3ac47e48dae3561644377caad4fff69747f1
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2022-07-14 13:46:07 -0300

    Add an unimplemented CSVUtils class. #166

---
+++ src/main/java/utils/csv/CSVUtils.java
@@ -0,0 +1,16 @@
+package utils.csv;
+
+/**
+ * This class provides facilities to use CSV.
+ */
+public class CSVUtils {
+
+    public static String[][] readCSV(String pathToFile) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void writeCSV(String pathToFile) {
+        throw new UnsupportedOperationException();
+    }
+
+}
