9febbc74925bfc114365fb25de2a5b8d13a57a59
yeografik <nachogonz2702@gmail.com>
2022-07-16 13:10:03 -0300

    Implemented methods of class ModelToForm #171

--- src/main/java/formatters/ModelToForm.java
+++ src/main/java/formatters/ModelToForm.java
@@ -2,26 +2,72 @@ package formatters;
 
 import models.*;
 
+import java.util.Map;
+
 public class ModelToForm {
 
     public static Object[] toForm(Ticket ticket) {
-        Object[] values = new Object[11];
-        values[0] = ticket.getValues().get("type");
-        return values;
+        Map<String, Object> dict = ticket.getValues();
+        Withholding withholding = (Withholding)dict.get("withholding");
+        dict.putAll(withholding.getValues());
+
+        Provider provider = (Provider)dict.get("provider");
+
+        String sector = (String)dict.get("sector");
+        if (sector == null) {   //in case ticket doesn't has a modified sector, we use provider sector
+            sector = (String) provider.getValues().get("sector");
+        }
+        Boolean delivered = (Boolean) (dict.get("delivered"));
+        String buyNSell = (boolean)dict.get("issuedByMe") ? "VENTA" : "COMPRA";
+
+        return new Object[] {dict.get("id"), dict.get("date"), dict.get("type"), dict.get("number"), dict.get("numberTo"),
+                dict.get("authCode"), provider.getValues().get("docNo"), provider.getValues().get("name"),
+                dict.get("exchangeType"), dict.get("netAmountWI"), dict.get("netAmountWOI"), dict.get("amountImpEx"),
+                dict.get("ivaTax"), dict.get("totalAmount"), sector, buyNSell, delivered ? "SI" : "NO"};
     }
     
     public static Object[] profitWithholdingToForm(Withholding withholding) {
-        throw new UnsupportedOperationException("TODO");
+        Map<String, Object> dict = withholding.getValues();
+        Object iva = dict.get("iva");
+        if (iva == null || (Float) iva == 0.0f) {   //in case there is no iva withholding return empty array
+            return new Object[0];
+        }
+
+        Provider provider = (Provider)dict.get("provider");
+        String sector = (String)dict.get("sector");
+        if (sector == null) {   //in case ticket doesn't have a modified sector, we use provider sector
+            sector = (String) provider.getValues().get("provSector");
+        }
+        Boolean delivered = (Boolean) (dict.get("delivered"));
+
+        return new Object[] {dict.get("id"), dict.get("date"), "Retencion Iva", dict.get("number"), null, null,
+                provider.getValues().get("docNo"), provider.getValues().get("name"), null, null, null, null, null, iva,
+                sector, null, delivered ? "SI" : "NO"};
     }
 
     public static Object[] IVAWithholdingToForm(Withholding withholding) {
-        throw new UnsupportedOperationException("TODO");
+        Map<String, Object> dict = withholding.getValues();
+        Object profits = dict.get("profits");
+        if (profits == null || (Float) profits == 0.0f) {   //in case there is no iva withholding return empty array
+            return new Object[0];
+        }
+
+        Provider provider = (Provider)dict.get("provider");
+        String sector = (String)dict.get("sector");
+        if (sector == null) {   //in case ticket doesn't have a modified sector, we use provider sector
+            sector = (String) provider.getValues().get("provSector");
+        }
+        Boolean delivered = (Boolean) (dict.get("delivered"));
+
+        return new Object[] {dict.get("id"), dict.get("date"), "Retencion Ganancias", dict.get("number"), null, null,
+                provider.getValues().get("docNo"), provider.getValues().get("name"), null, null, null, null, null,
+                profits, sector, null, delivered ? "SI" : "NO"};
     }
 
     public static Object[] toForm(Provider provider) {
-        Object[] values = new Object[11];
-        values[0] = provider.getValues().get("type");
-        return values;
+        Map<String, Object> dict = provider.getValues();
+        return new Object[] {dict.get("docNo"), dict.get("name"), dict.get("docType"), dict.get("address"),
+                dict.get("provSector"), dict.get("alias")};
     }
 
 }
