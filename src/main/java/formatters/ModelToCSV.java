f00a3037efaf80a9626350a57749cf2960e2227f
AFungo <augustofungo@gmail.com>
2022-07-18 18:56:01 -0300

    I implement toCSV for provider model. Ref #170

--- src/main/java/formatters/ModelToCSV.java
+++ src/main/java/formatters/ModelToCSV.java
@@ -61,8 +61,28 @@ public class ModelToCSV {
         return csvLine;    
     }
 
+     /**
+     * given a Provider return a CSV line representation of the Provider
+     * @param Provider to be transformed
+     * @return a CSV line representation of the Provider
+     */
     public static String[] toCSV(Provider provider) {
-        throw new UnsupportedOperationException("TODO");
+        Map<String, Object> values = provider.getValues();
+        Set<String> attributes = Provider.getAttributes();
+
+        String[] csvLine = new String[attributes.size()];
+
+        Sector sector = (Sector)values.get("provSector");
+
+        csvLine[0] = sector.getID().get("id").toString();//add sector to csvLine
+        attributes.remove("provSector");// remove sector from attributes
+
+        int i = 1;
+        for (String att : attributes) {
+            csvLine[i] = values.get(att).toString(); 
+            i++;     
+        }
+        return csvLine;
     }
 
 }
