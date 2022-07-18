3a957a3de71ce801b454d5e8167c125f2b686395
AFungo <augustofungo@gmail.com>
2022-07-18 18:45:14 -0300

    I implement toCSV for withholdings. Ref #170

--- src/main/java/formatters/ModelToCSV.java
+++ src/main/java/formatters/ModelToCSV.java
@@ -24,7 +24,7 @@ public class ModelToCSV {
 
 
         csvLine[0] = ticket.getID().get("id").toString();//add the id of the withholding 
-        attributes.remove(values.get("withholding"));//remove withholding of the set of attributes
+        attributes.remove("withholding");//remove withholding of the set of attributes
         int i = 1;
         for (String att : attributes) {
             csvLine[i] = values.get(att).toString(); 
@@ -33,8 +33,32 @@ public class ModelToCSV {
         return csvLine;    
     }
 
+    /**
+     * given a withholding return a CSV line representation of the withholding
+     * @param withholding to be transformed
+     * @return a CSV line representation of the withholding
+     */
     public static String[] toCSV(Withholding withholding) {
-        throw new UnsupportedOperationException("TODO");
+        Map<String, Object> values = withholding.getValues();
+        Set<String> attributes = Withholding.getAttributes();
+
+        String[] csvLine = new String[attributes.size()];
+
+        Provider provider = (Provider)values.get("provider");
+        Sector sector = (Sector)values.get("sector");
+
+        csvLine[0] = provider.getID().get("id").toString();//add provider to csvLine
+        attributes.remove("provider");//remove provider from attributes
+
+        csvLine[1] = sector.getID().get("id").toString();//add sector to csvLine
+        attributes.remove("sector");// remove sector from attributes
+
+        int i = 2;
+        for (String att : attributes) {
+            csvLine[i] = values.get(att).toString(); 
+            i++;     
+        }
+        return csvLine;    
     }
 
     public static String[] toCSV(Provider provider) {
